8e146eb6b6808bd7c6d2d5c9495b4572794fcec4
Aarne Talman <aarne.talman@amd.com>
2026-01-17 11:57:06 +0000

    Update website

---
+++ main.js
@@ -0,0 +1,8 @@
+// Mobile menu: close on link click
+document.querySelectorAll(".nav-mobile .menu a").forEach(a => {
+  a.addEventListener("click", () => {
+    const details = a.closest("details");
+    if (details) details.open = false;
+  });
+});
+
